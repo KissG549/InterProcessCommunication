83e0518e350c2845e374657e4a1b65952a376b6e
KissG549 <57135398+KissG549@users.noreply.github.com>
2020-11-17 17:22:40 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,31 +4,65 @@ namespace InterProcessCommunication
 {
     class Program
     {
+        static void printUsage()
+        {
+            Console.WriteLine(
+                "This program purpose is to demonstrate data exchange between two processes using bidirectional communication.\n" +
+                "Implemented data exchange method: Socket-JSON over TCP\n\n" +
+                "Usage:\n" +
+                "\t-c server_host:port_number\t\tRun in client mode, connect to the specific address:port" +
+                "\t\t -c 10.0.0.1:3001\n" +
+                "\t-s listening_address port_number\t\tRun in server mode, listening on specific IP and port" +
+                "\t\t -s 127.0.0.1 3001" +
+                "\t\t -s 3001" +
+                "\t?\t\tDisplay this message.");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var client = new ClientImpl();
 
-            try
+            if( args.Length < 2 )
             {
-                client.connect("127.0.0.1:3000");
-                // connectionList.append( client.getConnectionId() );
+                Console.WriteLine("You missed the program parameters!");
+                printUsage();
             }
-            catch(Exception e)
+            else if( args[0] == "-c" )
             {
-                Console.WriteLine("Failed to connect to the server, ", e.ToString());
+                var client = new ClientImpl();
+
+                try
+                {
+                    client.Connect(args[1]);
+                    // connectionList.append( client.getConnectionId() );
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Failed to connect to the server: {0}", e.ToString());
+                }
+
+                if (client.IsConnected)
+                {
+                    // client.asyncWaitForMsg(dataProcessCallback);
+                    client.AsyncWaitForInput();
+                }
+            }
+            else if( args[0] == "-s" )
+            {
+                ServerImpl server = new ServerImpl();
+                if (args.Length < 3)
+                {
+                    server.Listening("", Int32.Parse(args[1]));
+                }
+                else
+                {
+                    server.Listening(args[1], Int32.Parse(args[2]));
+                }
+            }
+            else
+            {
+                printUsage();
             }
 
-            if ( client.IsConnected )
-            {
-                /*
-                    dataProcessCallback purpose:
-                        - process incoming data
-                        - call Log function to print out the incoming information
-                 */
-                // client.asyncWaitForMsg(dataProcessCallback);
-                client.asyncWaitForInput();
-            }
         }
     }
 }
