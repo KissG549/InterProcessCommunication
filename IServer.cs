4f82683f872e1d3e8af7e5c6913367b7230e3fa7
KissG549 <57135398+KissG549@users.noreply.github.com>
2020-11-17 17:22:34 +0100

    Create IServer.cs

---
+++ IServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace InterProcessCommunication
+{
+    interface IServer
+    {
+        abstract void Listening(string pIPAddress, int pPortNumber);
+        abstract void AcceptCallback(IAsyncResult pAr);
+        abstract void ReadCallback(IAsyncResult pAr);
+
+        abstract void Send(Socket pHandler, string data);
+
+        abstract void SendCallBack(IAsyncResult pAr);
+    }
+}
