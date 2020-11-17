48be4cd3f1d29d2c441d52ab0ddcfa341834d9be
KissG549 <57135398+KissG549@users.noreply.github.com>
2020-11-17 17:22:28 +0100

    Create IDataEncoder.cs

---
+++ IDataEncoder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterProcessCommunication
+{
+    interface IDataEncoder
+    {
+        abstract byte[] Encapsulate(string pData);
+        abstract string Decapsulate(byte[] pData);
+    }
+}
