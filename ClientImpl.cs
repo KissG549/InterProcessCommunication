d4e249a92d5ad2f08f20a47617b22d725355f01d
KissG549 <57135398+KissG549@users.noreply.github.com>
2020-11-17 12:59:06 +0100

    Initial commit

---
+++ ClientImpl.cs
@@ -0,0 +1,61 @@
+﻿using InterProcessCommunication.InterProcessCommunication;
+using System;
+
+namespace InterProcessCommunication
+{
+    internal class ClientImpl : IClient
+    {
+        private int mConnectionId;
+        private bool mIsConnected;
+        private string mServerAddr;
+
+        public ClientImpl()
+        {
+            mConnectionId = 0;
+            mIsConnected  = false;
+            mServerAddr = "";
+        }
+
+        ~ClientImpl()
+        {
+            if (mIsConnected) { disconnect(); }
+        }
+
+        public int ConnectionId
+        {
+            get { return mConnectionId;  }
+        }
+
+        public bool IsConnected
+        {
+            get { return mIsConnected;  }
+        }
+
+        public string ServerAddr
+        {
+            get { return mServerAddr; }
+        }
+
+        public void asyncWaitForMsg()
+        {
+            Console.WriteLine("asyncWaitForMsg from the other end");
+        }
+
+        public void asyncWaitForInput()
+        {
+            Console.WriteLine("asyncWaitForInput from the user ");
+        }
+
+        public bool connect(string pServerAddr)
+        {
+            Console.WriteLine("Connecting to server: ", pServerAddr);
+            return true;
+        }
+
+        public bool disconnect()
+        {
+            Console.WriteLine("Disconnecting");
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InterProcessCommunication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var client = new ClientImpl();
+
+            try
+            {
+                client.connect("127.0.0.1:3000");
+                // connectionList.append( client.getConnectionId() );
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Failed to connect to the server, ", e.ToString());
+            }
+
+            if ( client.IsConnected )
+            {
+                /*
+                    dataProcessCallback purpose:
+                        - process incoming data
+                        - call Log function to print out the incoming information
+                 */
+                // client.asyncWaitForMsg(dataProcessCallback);
+                client.asyncWaitForInput();
+            }
+        }
+    }
+}
