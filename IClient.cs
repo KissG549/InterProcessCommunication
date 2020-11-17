c9f389d57317bb42ce45551fce9a02d1de7736dc
KissG549 <57135398+KissG549@users.noreply.github.com>
2020-11-17 13:05:29 +0100

    Create IClient.cs

---
+++ IClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterProcessCommunication.InterProcessCommunication
+{
+    internal interface IClient
+    {
+        bool connect(string pServerAddr);
+        bool disconnect();
+        void asyncWaitForMsg(); // Wait messages from the server
+        void asyncWaitForInput();  // Wait messages from the user
+        /*
+            Properties
+         */
+        int ConnectionId
+        {
+            get;
+        }
+
+        bool IsConnected
+        {
+            get;
+        }
+
+        string ServerAddr
+        {
+            get;
+        }
+    }
+}
