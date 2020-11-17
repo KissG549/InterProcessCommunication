e1476efbbcecabf6e29ebac004a582a1d377d043
KissG549 <57135398+KissG549@users.noreply.github.com>
2020-11-17 17:22:09 +0100

    DataEncoderImpl added

---
+++ DataEncoderImpl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace InterProcessCommunication
+{
+    class DataEncoderImpl : IDataEncoder
+    {
+        public byte[] Encapsulate(string pData)
+        {
+            string jsonString;
+            jsonString = JsonSerializer.Serialize(pData);
+
+            byte[] data = Encoding.Unicode.GetBytes(jsonString);
+            // TODO
+            return data;
+        }
+        public string Decapsulate(byte[] pData)
+        {
+            string dataString;
+            dataString = Encoding.Unicode.GetString(pData);
+
+            return JsonSerializer.Deserialize<string>(dataString);
+        }
+
+    }
+}
